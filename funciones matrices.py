ada4820037bcc0d352f5fcb43be0927b7de3c10f
Jose Miguel Erazo Ayón <46788386+josemerazo@users.noreply.github.com>
2023-08-02 10:58:04 -0500

    Add files via upload

--- funciones matrices.py
+++ funciones matrices.py
@@ -31,10 +31,23 @@ def transponer_matrices(matriz):
             fila_transpuesta.append(matriz[i][j])
         matriz_transpuesta.append(fila_transpuesta)
     return matriz_transpuesta
+
+def suma_matrices(matriz1, matriz2):
+    filas = len(matriz1)
+    columnas = len(matriz1[0])
+    
+    matriz_respuesta = []
+    for i in range(filas):
+        fila_suma = []
+        for j in range(columnas):
+            suma_elemento=matriz1[i][j] + matriz2[i][j] 
+            fila_suma.append(suma_elemento)
+        matriz_respuesta.append(fila_suma)
+    return matriz_respuesta
     
         
 matriz1= [[2,8],[4,10]]
-matriz2= [[5,3,7],[4,2,9],[8,3,1]]
+matriz2= [['a','b','c'],['d','e','f'],['g','h','i']]
  
 print("Matriz 1: ")
 imprime_matriz(matriz1)
@@ -51,9 +64,11 @@ matriz3 = ingresa_matriz(2,3)
 imprime_matriz(matriz3)
 """
 
-matriz_nueva = ingresa_matriz(3,3)
+matriz_nueva = ingresa_matriz(2,2)
 matriz_nueva_transpuesta = transponer_matrices(matriz_nueva)
 imprime_matriz(matriz_nueva_transpuesta)
 #en una sola linea
-imprime_matriz(transponer_matrices(ingresa_matriz(2,2)))
+#imprime_matriz(transponer_matrices(ingresa_matriz(2,2)))
+
+imprime_matriz(suma_matrices(matriz_nueva, matriz1))
 
