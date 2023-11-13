547240fdc40a8fc6d910099452a1a80d47de7acb
Jose Miguel Erazo Ayón <46788386+josemerazo@users.noreply.github.com>
2023-11-13 15:40:53 -0500

    Menu dinámico

---
+++ menu.py
@@ -0,0 +1,16 @@
+opcion = "0"
+while opcion!="4":
+
+    print("1.- Opcion 1 ")
+    print("2.- Opcion 2 ")
+    print("3.- Opcion 3 ")
+    print("4.- Opcion 4 ")
+    opcion = input("Ingrese opcion: ")
+    if(opcion=="1"):
+        print("Ha seleccionado opcion 1")
+    elif(opcion=="2"):
+        print("Ha seleccionado opcion 2")
+    elif(opcion=="3"):
+        print("Ha seleccionado opcion 3")
+    elif(opcion=="4"):
+        print("Ha salido del programa")
